743da698520f099391b5a3b589ba47f8bd0f91db
echeadle <echeadle@gmail.com>
2023-09-02 09:06:34 -0600

    Section 26 finish employee view.

--- django-basic/employees/views.py
+++ django-basic/employees/views.py
@@ -5,4 +5,4 @@ from employees.models import Employee
 
 def employee_detail(request, pk):
     employee = get_object_or_404(Employee, pk=pk)
-    return HttpResponse(employee)
+    return render(request, 'employee_detail.html')
